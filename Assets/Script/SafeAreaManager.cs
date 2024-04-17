726876791372437b6152623ff8f13f71289ac8c3
Nathan Bellaiche <nathanbellaiche000@gmail.com>
2024-04-17 19:23:21 +0200

    ProjectCommit

---
+++ Assets/Script/SafeAreaManager.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class SafeAreaManager : MonoBehaviour
+{
+    public static Rect GetSafeRect()
+    {
+        Vector2 start = Vector2.zero;
+        Vector2 size = Vector2.zero;
+        
+        
+
+        return new Rect(start, size);
+    }
+}
